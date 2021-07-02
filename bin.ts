4713a3368303b41c29e4b01aec8e676babe07717
Cotton Hou <himcotton@gmail.com>
2021-07-03 02:44:43 +0800

    upgrade cliffy@v0.19.2

--- bin.ts
+++ bin.ts
@@ -1,6 +1,6 @@
 import { main } from './mod.ts'
 
-import { Command } from 'https://deno.land/x/cliffy@v0.18.2/command/mod.ts';
+import { Command } from 'https://deno.land/x/cliffy@v0.19.2/command/mod.ts';
 
 
 
