df8ef247a591dbf1601cc6307321b44a27cd2142
Cotton Hou <himcotton@gmail.com>
2022-10-04 10:17:46 +0800

    port verify

--- mod.test.ts
+++ mod.test.ts
@@ -3,6 +3,7 @@ import * as ast from 'https://deno.land/std@0.158.0/testing/asserts.ts';
 import {
 
     port_normalize,
+    port_verify,
 
 } from './mod.ts';
 
@@ -24,6 +25,35 @@ Deno.test('port_normalize', () => {
 
     eq('https://example.com:80', 80);
     eq('http://example.com:443', 443);
+
+});
+
+
+
+
+
+Deno.test('port_verify', () => {
+
+    const eq = (n: unknown, res: number | undefined, msg = n) => {
+        ast.assertStrictEquals(port_verify(n), res, `${ msg }`);
+    };
+
+    const eq_undefined = (n: unknown) => eq(n, undefined);
+
+
+
+    eq( 80,  80);
+    eq(443, 443);
+
+    eq_undefined(true);
+    eq_undefined(false);
+    eq_undefined('');
+    eq_undefined('42');
+    eq_undefined(NaN);
+    eq_undefined(2n);
+    eq_undefined(-1);
+    eq_undefined(4.2);
+    eq_undefined(999999);
 
 });
 
